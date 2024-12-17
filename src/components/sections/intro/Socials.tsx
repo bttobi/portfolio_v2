96f7411b14652e5d146bb21726a5784e015b6c1e
bttobi <bttobi123@gmail.com>
2024-12-17 23:47:33 +0100

    fix: fixed icon colors

--- src/components/sections/intro/Socials.tsx
+++ src/components/sections/intro/Socials.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Icon, IconType } from "../../icons";
 
 const socials = [
-  { text: "Email", icon: "", color: "" },
-  { text: "LinkedIn", icon: "", color: "" },
-  { text: "Github", icon: "", color: "" },
-  { text: "Resume", icon: "", color: "" },
+  { text: "Email", icon: "", color: "#8C00A2", link: "" },
+  { text: "LinkedIn", icon: "", color: "#0072A9", link: "" },
+  { text: "Github", icon: "", color: "#000", link: "" },
+  { text: "Resume", icon: "", color: "#009886", link: "" },
 ];
 
 const Socials = () => {
@@ -12,14 +13,27 @@ const Socials = () => {
     <div className="flex flex-col justify-center items-center">
       <img
         className="rounded-full w-36 mt-10 mb-4"
-        src="../../../../public/pictures/profile_picture.jpg"
+        src="./pictures/profile_picture.jpg"
         alt="Bartosz Tobiński's face"
       />
       <div className="flex gap-4">
         {socials.map((social) => (
-          <div>
-            <div>{social.text}</div>
-          </div>
+          <a
+            className="flex flex-col justify-center items-center"
+            href={social.link}
+            target="_blank"
+            key={social.text}
+          >
+            <div
+              style={{ backgroundColor: social.color }}
+              className={
+                "flex justify-center items-center w-10 h-10 rounded-full"
+              }
+            >
+              <Icon type={IconType.MENU} />
+            </div>
+            {social.text}
+          </a>
         ))}
       </div>
     </div>
